6567405f441d5ae21f7205b840b2f95e408f2646
Gyungah Cho <hellen110@naver.com>
2019-07-16 10:11:25 +0900

    Create star.py

---
+++ 1주차/star.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a=1
+
+
+# In[2]:
+
+
+_b=2
+
+
+# In[6]:
+
+
+c='경아'
+
+
+# In[7]:
+
+
+def x():
+    return 1
+
+
+# In[8]:
+
+
+def _y():
+    return 2
+
+
+# In[9]:
+
+
+__all__=['a']
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
